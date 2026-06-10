a232a355beed266dd80abd4cd701399f76e560f8
Manoel <manoel.carvalhoneto@tutamail.com>
2026-06-10 13:56:11 -0300

    chore: ignore generated build artifacts

--- core/xml/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const XML_MEDIA_TYPE = "application/xml";
-export declare const escapeXmlText: (value: string) => string;
-export declare const encodeUtf8: (value: string) => Uint8Array;
-export declare const getUtf8ByteLength: (value: string) => number;
